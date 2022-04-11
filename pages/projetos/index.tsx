54dade6328c0809723baa1090c8fe0b21a17ee64
Brayan Wilis <BrayanWilis@outlook.com>
2022-04-11 19:08:21 -0300

    [ Fix: Projetos ]: excluded the loading and changed to Next Image Component

--- pages/projetos/index.tsx
+++ pages/projetos/index.tsx
@@ -1,5 +1,5 @@
 import NextHead from "../../src/components/defaultPage/NextHead";
-import { useSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
 import { Grid, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
 import Menu from "../../src/components/defaultPage/Menu";
@@ -9,11 +9,10 @@ import { projetos } from "../../testdata";
 import LinearLoading from "../../src/components/LinearLoading";
 import CardCriarObra from "../../src/components/Obra/CriarObra";
 
-export default function Projetos({}) {
+export default function Projetos({ session }) {
     const [loading, setLoading] = useState(true);
     const [listaObras, setListaObras] = useState(projetos);
     const router = useRouter();
-    const { data: session } = useSession();
 
     // useEffect(() => {
     //     session ? setLoading(false) : router.push("/login");
@@ -22,48 +21,59 @@ export default function Projetos({}) {
     return (
         <>
             <NextHead title="Piarq | Projetos" />
-            {loading ? (
+            {/* {loading ? (
                 <LinearLoading />
             ) : (
-                <>
-                    <Menu image={session?.user?.image} />
-                    <Grid
-                        container
-                        direction="column"
-                        justifyContent="center"
-                        alignItems="center"
-                        alignContent="center"
-                    >
-                        <Grid
-                            container
-                            direction="column"
-                            justifyContent="center"
-                            alignItems="center"
-                            alignContent="center"
-                            wrap="wrap"
-                            sx={{ width: 280, height: 280 }}
-                        >
-                            <Typography fontFamily={"Pacifico"} fontSize={60}>
-                                Projetos
-                            </Typography>
-                        </Grid>
-                        <Grid
-                            container
-                            direction="row"
-                            justifyContent="flex-start"
-                            alignItems="flex-start"
-                            alignContent="flex-start"
-                            wrap="wrap"
-                            sx={{ width: "90vw", height: "100%" }}
-                        >
-                            {listaObras.map((item, index) => (
-                                <CardObra data={item} key={index} />
-                            ))}
-                            <CardCriarObra userEmail={session?.user?.email} />
-                        </Grid>
-                    </Grid>
-                </>
-            )}
+                <> */}
+            <Menu image={session?.user?.image} />
+            <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                alignContent="center"
+            >
+                <Grid
+                    container
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    alignContent="center"
+                    wrap="wrap"
+                    sx={{ width: 280, height: 280 }}
+                >
+                    <Typography fontFamily={"Pacifico"} fontSize={60}>
+                        Projetos
+                    </Typography>
+                </Grid>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="flex-start"
+                    alignContent="flex-start"
+                    wrap="wrap"
+                    sx={{ width: "90vw", height: "100%" }}
+                >
+                    {listaObras.map((item, index) => (
+                        <CardObra data={item} key={index} />
+                    ))}
+                    <CardCriarObra userEmail={session?.user?.email} />
+                </Grid>
+            </Grid>
+            {/* </>
+            )} */}
         </>
     );
+}
+
+export async function getServerSideProps({ context }) {
+    try {
+        const session = await getSession();
+        return {
+            props: { session },
+        };
+    } catch (err) {
+        console.error(err);
+    }
 }
